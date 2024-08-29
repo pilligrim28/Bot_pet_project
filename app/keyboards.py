8eca887b4517619612a37789e8fa46bd260cb959
pilligrim28 <balu-133@ya.ru>
2024-08-29 21:47:54 +0300

    Клавиатура + Readme

--- app/keyboards.py
+++ app/keyboards.py
@@ -6,4 +6,9 @@ main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог')],
                                       [KeyboardButton(text='Контакты'),
                                        KeyboardButton(text='О нас')]],
                                        resize_keyboard=True,
-                            
+                                      input_field_placeholder='Выберите пункт меню' )
+
+
+catalog = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Футболки')],
+                                                [InlineKeyboardButton('Кросовки')],
+                                                [InlineKeyboardButton(text='Кепки')]])
