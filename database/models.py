57377f54b5b76f8cb892b8968f94249e24c79047
pilligrim28 <balu-133@ya.ru>
2024-08-31 18:53:18 +0300

    добавить

---
+++ database/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+engine = create_async_engine(url = 'sqlite-aiosqlite:///db.sqlite3')
+
+async_session = async_sessionmaker(engine)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ =
