9a1ec9a35282238cc17af02e8094b0c517082cbe
pilligrim28 <balu-133@ya.ru>
2024-09-02 14:24:07 +0300

    123

--- database/models.py
+++ database/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy import BigInteger
+
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
@@ -9,4 +11,20 @@ class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 class User(Base):
-    __tablename__ =
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id =mapped_column(BigInteger)
+
+
+class Category(Base):
+    __tablename__ ='categories'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+
+
+class Item(Base):
+    __tablename__ ='items'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
